e7881b83a4e864baac7ca996cee7e44e83c7898e
mdabidind <booksofkid@gmail.com>
2025-07-16 16:51:49 +0530

    Update download.py

--- scripts/download.py
+++ scripts/download.py
@@ -1,10 +1,13 @@
-import sys
 import requests
+import sys
 
-def download_pdf(url, save_path):
-    response = requests.get(url)
-    with open(save_path, 'wb') as f:
-        f.write(response.content)
+def download_file(url, filename):
+    headers = {'User-Agent': 'PDFConverter/1.0'}
+    with requests.get(url, headers=headers, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
 
 if __name__ == "__main__":
-    download_pdf(sys.argv[1], sys.argv[2])
+    download_file(sys.argv[1], sys.argv[2])
