3300ef8d927a1b0a8c2d6cab40783b9a44ededdd
mdabidind <booksofkid@gmail.com>
2025-07-16 16:44:56 +0530

    Update download.py

--- scripts/download.py
+++ scripts/download.py
@@ -1,1 +1,10 @@
+import sys
+import requests
 
+def download_pdf(url, save_path):
+    response = requests.get(url)
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+
+if __name__ == "__main__":
+    download_pdf(sys.argv[1], sys.argv[2])
